4e77d7ffc47c36751519397bdb4df60c34d8c54c
sgeisert <sgeisert@WSCOM012>
2022-09-22 10:50:15 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿/* Questions for Vector:
+ * 
+    * What's the difference between a position/classification and a location?
+    * How do you disassociate a position from a user?  
+    * How many distinct labels can you apply to one user?  Is there a limit?
+    * You can’t update a position or location via your API for a person as far as I can tell
+    * How do you activate someone via the API?
+ */
+
+var vector = new Vector();
+
+Console.WriteLine("---------- Adding New Parent Classification/Position ---------- ");
+var newPosition = await vector.AddPosition("Campus", "campus", Guid.Empty);
+Console.WriteLine("Successfully created parent position: " + newPosition.PositionId);
+
+Console.WriteLine();
+Console.WriteLine("---------- Adding New Child Classification/Position ---------- ");
+var newChildPosition = await vector.AddPosition("Online", "online", new Guid(newPosition.PositionId));
+Console.WriteLine("Successfully created child position: " + newPosition.PositionId);
+
+Console.WriteLine();
+Console.WriteLine("---------- Getting All Classifications/Positions ---------- ");
+var positions = await vector.GetAllPositions();
+foreach (var position in positions)
+{
+    Console.WriteLine(position.PositionId + ": " + position.Name);
+}
+
+Console.WriteLine();
+Console.WriteLine("---------- Adding a New User w/ Position Id We Just Created ---------- ");
+var newUser = new Person {
+    ExternalUniqueId = "820182056",
+    FirstName = "Joy",
+    LastName = "Akey",
+    UserName = "jakey",
+    PositionId = new Guid(newChildPosition.PositionId)
+};
+var createdUser = await vector.AddUser(newUser);
+
+Console.WriteLine();
+Console.WriteLine("---------- Getting First 100 Active Users ---------- ");
+var activeUsers = await vector.GetFirst100ActivePeopleAndProgress();
+foreach (var person in activeUsers)
+{
+    Console.WriteLine(person.PersonId + ": " + person.Username + " " + person.First + " " + person.Last);
+}
+
+Console.WriteLine();
+Console.WriteLine("---------- Getting First User From Active User List ---------- ");
+var user = await vector.GetUserByGuid(new Guid(activeUsers[0].PersonId));
+Console.WriteLine(user.PersonId + ": " + user.Username + " " + user.First + " " + user.Last);
+
+
+Console.WriteLine();
+Console.WriteLine("---------- Updating First Name For That User ---------- ");
+var p = new Person(user);
+p.FirstName = p.FirstName + "2";
+var updatedUser = await vector.UpdateUser(p);
+Console.WriteLine(user.PersonId + ": " + user.Username + " " + user.First + " " + user.Last);
+
+Console.WriteLine();
+Console.WriteLine("---------- Deactivate the User We Just Updated ---------- ");
+var deactivatedUser = await vector.DeactivatePersonById(new Guid(user.PersonId));
+Console.WriteLine("Successfully deactivated: " + user.PersonId);
+return;
---
+++ Vector.cs
@@ -0,0 +1,211 @@
+﻿using System.Configuration;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+using VectorGraphQL;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+internal class Vector
+{
+    private static Token _bearer;
+    private static IVectorClient _client;
+
+    public Vector()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        // Inspired by https://chillicream.com/docs/strawberryshake/networking/authentication
+        serviceCollection
+            .AddVectorClient()
+            .ConfigureHttpClient(_client =>
+            {
+                _client.BaseAddress =
+                    new Uri(ConfigurationManager.AppSettings["VectorEndPoint"]);
+                _client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", GetTokenAsync(_client).Result.AccessToken);
+            });
+
+        IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+        _client = services.GetRequiredService<IVectorClient>();
+    }
+
+    /// <summary>
+    /// Method to return a bearer token
+    /// </summary>
+    /// <param name="client"></param>
+    /// <returns></returns>
+    public async Task<Token> GetTokenAsync(HttpClient client)
+    {
+        string baseAddress = ConfigurationManager.AppSettings["VectorTokenEndPoint"];
+        string grant_type = "client_credentials";
+        string client_id = ConfigurationManager.AppSettings["ClientID"];
+        string client_secret = ConfigurationManager.AppSettings["ClientSecret"];
+
+        var form = new Dictionary<string, string>
+                {
+                    {"grant_type", grant_type},
+                    {"client_id", client_id},
+                    {"client_secret", client_secret},
+                };
+
+        HttpResponseMessage tokenResponse = await client.PostAsync(baseAddress, new FormUrlEncodedContent(form));
+        var jsonContent = await tokenResponse.Content.ReadAsStringAsync();
+        var tok = JsonSerializer.Deserialize<Token>(jsonContent);
+        return tok;
+    }
+
+    #region "Person-Related Methods"
+    public async Task<IReadOnlyList<IGetFirst100People_People_Nodes>> GetFirst100People()
+    {
+        IOperationResult<IGetFirst100PeopleResult> result = await _client.GetFirst100People.ExecuteAsync();
+        result.EnsureNoErrors();
+
+        return result.Data.People.Nodes;
+    }
+
+    public async Task<IReadOnlyList<IGetFirst100ActivePeopleAndProgress_People_Nodes>> GetFirst100ActivePeopleAndProgress()
+    {
+        IOperationResult<IGetFirst100ActivePeopleAndProgressResult> result = await _client.GetFirst100ActivePeopleAndProgress.ExecuteAsync();
+        result.EnsureNoErrors();
+
+        return result.Data.People.Nodes;
+    }
+    public async Task<IGetPersonById_Person> GetUserByGuid(Guid personId)
+    {
+        IOperationResult<IGetPersonByIdResult> result = await _client.GetPersonById.ExecuteAsync(personId.ToString().ToUpper());
+        result.EnsureNoErrors();
+
+        return result.Data.Person;
+    }
+
+    public async Task<IAddPerson_AddPerson> AddUser(Person person)
+    {
+        IOperationResult<IAddPersonResult> result;
+
+        if (person.PositionId == Guid.Empty)
+            result = await _client.AddPerson.ExecuteAsync(person.Address1,
+            person.Address2,
+            person.BeginDate,
+            person.Email,
+            person.ExternalUniqueId,
+            person.FirstName,
+            person.LastName,
+            person.Phone,
+            null,
+            person.UserName
+            );
+        else
+            result = await _client.AddPerson.ExecuteAsync(person.Address1,
+            person.Address2,
+            person.BeginDate,
+            person.Email,
+            person.ExternalUniqueId,
+            person.FirstName,
+            person.LastName,
+            person.Phone,
+            person.PositionId.ToString().ToUpper(),
+            person.UserName
+            );
+
+        result.EnsureNoErrors();
+
+        return result.Data.AddPerson;
+    }
+
+    public async Task<IUpdatePerson_Person> UpdateUser(Person person)
+    {
+        IOperationResult<IUpdatePersonResult> result = await _client.UpdatePerson.ExecuteAsync(person.Address1,
+            person.Address2,
+            person.Email,
+            person.FirstName,
+            person.LastName,
+            person.PersonId.ToString().ToUpper(),
+            person.Phone,
+            person.UserName);
+        result.EnsureNoErrors();
+
+        return result.Data.Person;
+    }
+
+    public async Task<IDeactivatePerson_Person> DeactivatePersonById(Guid personId)
+    {
+        IOperationResult<IDeactivatePersonResult> result = await _client.DeactivatePerson.ExecuteAsync(personId.ToString().ToUpper());
+        result.EnsureNoErrors();
+
+        return result.Data.Person;
+    }
+
+    #endregion
+
+    #region "Position-Related Methods"
+    public async Task<IReadOnlyList<IGetAllPositions_Positions_Nodes>> GetAllPositions()
+    {
+        IOperationResult<IGetAllPositionsResult> result = await _client.GetAllPositions.ExecuteAsync();
+        result.EnsureNoErrors();
+
+        return result.Data.Positions.Nodes;
+    }
+
+    public async Task<IAddPosition_AddPosition> AddPosition(string name, string code, Guid parentId)
+    {
+        IOperationResult<IAddPositionResult> result;
+
+        if (parentId == Guid.Empty)
+            result = await _client.AddPosition.ExecuteAsync(name, code, null);
+        else
+            result = await _client.AddPosition.ExecuteAsync(name, code, parentId.ToString().ToUpper());
+
+        result.EnsureNoErrors();
+
+        return result.Data.AddPosition;
+    }
+    #endregion
+
+}
+
+public class Person
+{
+    public Person()
+    {
+    }
+    public Person(IGetPersonById_Person person)
+    {
+        this.Address1 = person.Address1;
+        this.Address2 = person.Address2;
+        this.Email = person.Email;
+        this.ExternalUniqueId = person.ExternalUniqueId;
+        this.FirstName = person.First;
+        this.LastName = person.Last;
+        this.PersonId = new Guid(person.PersonId);
+        this.Phone = person.Phone;
+        this.UserName = person.Username;
+    }
+    public string Address1 { get; set; }
+    public string Address2 { get; set; }
+    public string BeginDate { get; set; }
+    public string Email { get; set; }
+    public string ExternalUniqueId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Guid PersonId { get; set; }
+    public string Phone { get; set; }
+    public Guid PositionId { get; set; }
+    public string UserName { get; set; }
+}
+
+internal class Token
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; set; }
+}
